7e59d067e19a26865f3dbc3e0a4c1e42baa9e742
griffinkoh <s10223376@connect.np.edu.sg>
2022-05-08 01:19:56 +0800

    Finished practical 3

---
+++ app/src/main/java/sg/edu/np/mad/MADPractical/MainActivity.java
@@ -0,0 +1,45 @@
+package sg.edu.np.mad.MADPractical;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+
+
+public class MainActivity extends AppCompatActivity {
+
+
+    User newUser = new User("Griffin", "Year 2 student in School of Infocomm Technology", 1, true );
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        TextView username = findViewById(R.id.user);
+        Intent receive = getIntent();
+        int newInput = receive.getIntExtra("random", 1739039545);
+        username.setText(newUser.Name + " " + newInput);
+
+        TextView description = findViewById(R.id.description);
+        description.setText(newUser.Description);
+    }
+
+    public void followCheck(View view){
+        Button follow = findViewById(R.id.followButton);
+        if (newUser.Followed == true){
+            follow.setText("Unfollowed");
+            newUser.Followed = false;
+            Toast.makeText(MainActivity.this, "Followed", Toast.LENGTH_SHORT).show();
+        }
+        else{
+            follow.setText("Followed");
+            newUser.Followed = true;
+            Toast.makeText(MainActivity.this, "Unfollowed", Toast.LENGTH_SHORT).show();
+        }
+    }
+}
